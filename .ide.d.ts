57cdd75b2e2f09d7f6aafea6dcd6afa9e4d5d031
MoeShin <hi@moeshin.com>
2022-06-10 00:37:59 +0800

    web: 运行状态切换

---
+++ .ide.d.ts
@@ -0,0 +1,5 @@
+declare type ApiResponse = {
+    ok: boolean
+    msg: string
+    data: any
+}
--- web/assets/script.js
+++ web/assets/script.js
@@ -3,18 +3,30 @@ class CxIm {
     static apiRequest(opts) {
         if (typeof opts !== 'object') {
             opts = {url: opts};
-        }
-        opts.dataType = 'json';
-        return $.ajax(opts)
-            .then(data => {
-                const msg = data['msg'];
-                if (data.ok) {
-                    if (msg) {
-                        console.warn(msg);
-                    }
-                    return data.data;
+        } else {
+            const obj = opts.params;
+            if (typeof obj === 'object') {
+                let url = new URL(opts.url, location.href);
+                const params = url.searchParams;
+                for (const k in obj) {
+                    params.set(k, obj[k]);
                 }
-                return Promise.reject(msg);
-            });
+                opts.url = url.toString();
+            }
+        }
+
+        opts.dataType = 'json';
+        return $.ajax(opts).then(this.handleApiResponse);
+    }
+    /** @param {ApiResponse} data */
+    static handleApiResponse(data) {
+        const msg = data.msg;
+        if (data.ok) {
+            if (msg) {
+                console.warn(msg);
+            }
+            return data.data;
+        }
+        return Promise.reject(msg);
     }
 }
