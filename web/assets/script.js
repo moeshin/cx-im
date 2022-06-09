dfe21d53a4a23e4cb4f6c0428e10b9dd2e8e43bb
MoeShin <hi@moeshin.com>
2022-06-09 13:29:42 +0800

    web: 用户列表

--- web/assets/script.js
+++ web/assets/script.js
@@ -1,1 +1,20 @@
 $ = mdui.$;
+class CxIm {
+    static apiRequest(opts) {
+        if (typeof opts !== 'object') {
+            opts = {url: opts};
+        }
+        opts.dataType = 'json';
+        return $.ajax(opts)
+            .then(data => {
+                const msg = data['msg'];
+                if (data.ok) {
+                    if (msg) {
+                        console.warn(msg);
+                    }
+                    return data.data;
+                }
+                return Promise.reject(msg);
+            });
+    }
+}
